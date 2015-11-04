0f7928d787c0a5fa0b2ed84d748a81c5046a4904
Takenori Sato <tsato@cloudian.com>
2015-11-04 10:41:03 +0900

    license under AGPL

--- build/run.sh
+++ build/run.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-java -cp .:./* com.cloudian.analytics.CloudSonar "$@"
+if [ -n "$CLOUDSONAR_JAVA_HOME" ]; then
+    # Why we can't have nice things: Solaris combines x86 and x86_64
+    # installations in the same tree, using an unconventional path for the
+    # 64bit JVM.  Since we prefer 64bit, search the alternate path first,
+    # (see https://issues.apache.org/jira/browse/CASSANDRA-4638).
+    for java in "$JAVA_HOME"/bin/amd64/java "$CLOUDSONAR_JAVA_HOME"/bin/java; do
+        if [ -x "$java" ]; then
+            JAVA="$java"
+            break
+        fi
+    done
+else
+    JAVA=java
+fi
+
+exec "$JAVA" -cp .:./* com.cloudian.analytics.CloudSonar "$@"
---
+++ src/com/cloudian/analytics/HTMAnomalyDetector.java
@@ -0,0 +1,11 @@
+package com.cloudian.analytics;
+
+public class HTMAnomalyDetector implements PollingUpdateHandler {
+
+	@Override
+	public void updateStatus(PollingJob job) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
